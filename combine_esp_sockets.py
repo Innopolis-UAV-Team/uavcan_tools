fc5f2b5c105586aceec20dbad2ac2f9338423681
ponomarevda <ponomarevda96@gmail.com>
2021-07-06 12:13:53 +0300

    improve logger and add processing of sigint and sigterm signals

--- combine_esp_sockets.py
+++ combine_esp_sockets.py
@@ -1,29 +1,57 @@
 #!/usr/bin/env python3
 import socket
+import threading
+import datetime
 
 ESP_PORT = 12345
 ESP_ADDRESSES = [
     "192.168.43.176",
     "192.168.43.132"
 ]
+
 esp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 esp_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 esp_sock.bind((socket.gethostname(), ESP_PORT))
 esp_sock.settimeout(0.001)
+esp_rx_counter = 0
 
 UAVCAN_GUI_TOOL_PORT = 12346
 uavcan_gui_tool_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 uavcan_gui_tool_sock.settimeout(0.001)
+local_rx_counter = 0
 
+def print_log():
+    global crnt_counter
+    global log_timer, esp_rx_counter, local_rx_counter
+    log_timer = threading.Timer(10.0, print_log).start()
+
+    if esp_rx_counter == 0:
+        esp_log_str = "{}esp_rx={}{}".format("\033[93m", esp_rx_counter, '\033[0m')
+    else:
+        esp_log_str = "esp_rx={}".format(esp_rx_counter)
+
+    if local_rx_counter == 0:
+        local_rx_str = "{}local_rx={}{}".format("\033[93m", local_rx_counter, '\033[0m')
+    else:
+        local_rx_str = "local_rx={}".format(local_rx_counter)
+
+    print("[{}] SLCAN Traffic Log: {}, {}".format(\
+          datetime.datetime.now().strftime("%H:%M:%S"),
+          esp_log_str,
+          local_rx_str))
+
+    esp_rx_counter = 0
+    local_rx_counter = 0
+
+log_timer = threading.Timer(10.0, print_log).start()
 try:
     while True:
-
         # ESP -> uavcan_gui_tool
         try:
             data, addr = esp_sock.recvfrom(1024)
             if len(data) != 0:
                 uavcan_gui_tool_sock.sendto(data, ("localhost", UAVCAN_GUI_TOOL_PORT))
-                # print(addr, len(data), "->", "localhost,", UAVCAN_GUI_TOOL_PORT)
+                esp_rx_counter += len(data)
         except socket.timeout as e:
             pass
 
@@ -33,13 +61,14 @@ try:
             if len(data) != 0:
                 for esp_addr in ESP_ADDRESSES:
                     esp_sock.sendto(data, (esp_addr, ESP_PORT))
-                    # print('uavcan_gui_tool', len(data), "->", esp_addr)
+                    local_rx_counter += len(data)
         except socket.timeout as e:
             pass
         except socket.gaierror as e:
             print(esp_addr, "socket.gaierror", e)
 
-except KeyboardInterrupt:
+except (KeyboardInterrupt, SystemExit):
     esp_sock.close()
     uavcan_gui_tool_sock.close()
+    log_timer.cancel()
     print('Interrupted by user, socket has been closed!')
--- create_serial_from_udp.sh
+++ create_serial_from_udp.sh
@@ -16,25 +16,22 @@ function check_processes_related_to_required_port {
 function concatenate_udp_and_serial_in_background {
     python3 combine_esp_sockets.py &
     sudo mkdir -p /dev/serial/by-id
-    sudo socat -v -d -d PTY,link=$VIRTUAL_SERIAL_PORT_NAME,raw,echo=0,b1000000 UDP4-LISTEN:$COMBINED_UDP_PORT,reuseaddr &
+    sudo socat PTY,link=$VIRTUAL_SERIAL_PORT_NAME,raw,echo=0,b1000000 UDP4-LISTEN:$COMBINED_UDP_PORT,reuseaddr &
     sleep 0.5
     sudo chmod 777 $VIRTUAL_SERIAL_PORT_NAME
 }
 function close_background_jobs() {
     echo ""
-    echo "Finishing... Closing all background jobs..."
-    jobs
-    kill %1
-    kill %2
-    kill %3
+    echo "$0 ($$). Finishing..."
+    pkill -P $$
     exit 0
 }
 
 
 cd "$(dirname "$0")"
 source config.sh
-echo "Starting... Pid of this process is" $$
-trap 'close_background_jobs' SIGINT
+echo "$0 ($$). Starting..."
+trap 'close_background_jobs' SIGINT SIGTERM
 check_processes_related_to_required_port
 concatenate_udp_and_serial_in_background
 [ ! -z "$DISPLAY" ] && uavcan_gui_tool --dsdl $CUSTOM_DSDL_PATH &
