3db316cdb137c1fa05c6b3a08f2c574ff6f1e3f7
ponomarevda <ponomarevda96@gmail.com>
2022-01-17 12:38:26 +0300

    clean up a little bit

--- slcan_parser.py
+++ slcan_parser.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 FRAME_MAX_LENGTH = 27   # size = 8
 FRAME_MIN_LENGTH = 11   # size = 0 (theoretical case)
-FRAME_SIZE_IDX = 9      # size = 0 (theoretical case)
+FRAME_SIZE_IDX = 9
 FRAME_FIRST_BYTE = 'T'
 FRAME_LAST_BYTE = '\r'
 
 
-def parse_data(storage_buffer, new_coming_bytes):
+def parse_data(storage, new_coming_bytes):
     """
     Brief. This function is manipulate with 3 types of buffer:
     - the storage buffer
@@ -16,21 +16,22 @@ def parse_data(storage_buffer, new_coming_bytes):
     it updates the storage and return the parsed frames.
     """
     parsed_frames = ""
-    updated_storage_buffer = ""
+    updated_storage = ""
 
-    data = storage_buffer + new_coming_bytes
+    data = storage + new_coming_bytes
+    combined_buffer_size = len(data)
 
-    if len(data) < FRAME_MIN_LENGTH:
-        updated_storage_buffer = data
+    if combined_buffer_size < FRAME_MIN_LENGTH:
+        updated_storage = data
     else:
         # Find all appropriate frames, skip bad frames.
         # Use while instead of for + range() because we need to increment by the different values
         head_idx = 0
         last_parsed_tail_idx = 0
-        while head_idx <= len(data) - FRAME_MIN_LENGTH:
+        while head_idx <= combined_buffer_size - FRAME_MIN_LENGTH:
             if data[head_idx] == FRAME_FIRST_BYTE:
                 frame_size = FRAME_MIN_LENGTH + 2 * int(data[head_idx + FRAME_SIZE_IDX])
-                if frame_size < len(data) - head_idx + 1 and data[head_idx + frame_size - 1] == FRAME_LAST_BYTE:
+                if frame_size < combined_buffer_size - head_idx + 1 and data[head_idx + frame_size - 1] == FRAME_LAST_BYTE:
                     parsed_frames += data[head_idx : head_idx + frame_size]
                     head_idx += frame_size
                     last_parsed_tail_idx = head_idx
@@ -38,6 +39,6 @@ def parse_data(storage_buffer, new_coming_bytes):
             head_idx += 1
 
         # put everything else into the storage buffer
-        updated_storage_buffer = data[last_parsed_tail_idx : len(data)]
+        updated_storage = data[last_parsed_tail_idx : combined_buffer_size]
 
-    return parsed_frames, updated_storage_buffer
+    return parsed_frames, updated_storage
